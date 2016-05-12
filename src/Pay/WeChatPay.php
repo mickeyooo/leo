39aa844ebca56d6bc639ecd1a875287e7e97c7d0
huangjianhua <fromhuang@msn.com>
2016-05-12 15:48:14 +0800

    updated: 修改curl问题

--- src/Pay/WeChatPay.php
+++ src/Pay/WeChatPay.php
@@ -203,13 +203,11 @@ class WeChatPay extends PayAbstract
     protected function post($xml, $url, $useCert = false, $second = 30)
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
         curl_setopt($ch, CURLOPT_TIMEOUT, $second);
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
-        // 严格校验
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         // 设置header
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         // 要求结果为字符串且输出到屏幕上
